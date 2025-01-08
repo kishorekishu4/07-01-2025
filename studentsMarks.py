a7a550cfae04394c5e55800025725dfb781fadb1
kishorekishu4 <kishorekishu969@gmail.com>
2025-01-08 05:43:26 +0000

    studentmarks

---
+++ studentsMarks.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+screen_time =[2,4,6]
+sleep_hours =[3,7,8]
+stu_name =["bhanu","prashu","suppu"]
+dict1 = {
+    "screen":screen_time,
+    "sleep_hours":sleep_hours,
+    "stu_name":stu_name
+    }
+
+print(dict1)
+df = pd.DataFrame(dict1)
+print(df)
+
